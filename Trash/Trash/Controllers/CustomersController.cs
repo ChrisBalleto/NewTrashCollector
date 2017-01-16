0a585307abbd7f3e6f996b2a2ae5a323241bae00
chrisballeto <chrisballeto@gmail.com>
2017-01-16 11:39:34 -0600

    added customers index

---
+++ Trash/Trash/Controllers/CustomersController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Web.Mvc;
+using Trash.Models;
+using Trash.ViewModels;
+
+namespace Trash.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customer
+        private ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public ActionResult Index()
+        {
+
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            return View(customers);
+        }
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+            return View("CustomerForm", viewModel);
+        }
+    }
+}
